b1f7b12889fd828f2f10bab199fdf20675b1f09f
RH Blanchfield <RHB@rhbmac.local>
2015-02-23 21:06:23 -0800

    Completed Arrays

--- BlocExercises/BlocExercises/StarTrekArrays.m
+++ BlocExercises/BlocExercises/StarTrekArrays.m
@@ -12,22 +12,50 @@
 
 - (NSArray *) arrayOfStarTrekCharactersFromString:(NSString *)characterString {
     /* WORK HERE */
-    return @[];
+    
+    //Turn String into array.
+    
+    NSString *testString = characterString;
+    //NSArray *arrayOfStarTrekCharactersFromString; = [testString componentsSeparatedByString:@","];
+    return [testString componentsSeparatedByString:@";"];
 }
+
+    //    Turn Array into String.
 
 - (NSString *) stringOfStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @"";
+    
+    NSArray *testArray = characterArray;
+    //NSString *stringOfStarTrekCharactersFromArray;
+    return [testArray componentsJoinedByString: @";"];
+    
+    
 }
+
+    //Sort Array Alaphabetically.
 
 - (NSArray *) alphabeticallySortedStarTrekCharactersFromArray:(NSArray *)characterArray {
     /* WORK HERE */
-    return @[];
+    
+    
+    NSArray *inputArray = characterArray;
+    return [inputArray sortedArrayUsingSelector:@selector(localizedCaseInsensitiveCompare:)];
 }
 
 - (BOOL) characterArrayContainsWorf:(NSArray *)characterArray {
     /* WORK HERE */
+    
+    
+    //Check for Worf presence (Y/N)
+    
+    
+    for (NSString *characterName in characterArray) {
+        NSString *worfName = characterName;
+        
+        if ([characterArray containsObject:worfName]) {
+            return YES;
+        }
+    }
+    
     return NO;
-}
-
-@end
+}@end
